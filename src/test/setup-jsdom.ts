c3fc73235a9d38dcc12bc36e85375f34e0730400
Bjorn Melin <bjornmelin16@gmail.com>
2026-04-07 02:39:40 -0600

    fix(test): align IntersectionObserver.scrollMargin mock default

--- src/test/setup-jsdom.ts
+++ src/test/setup-jsdom.ts
@@ -135,7 +135,7 @@ if (typeof window !== "undefined") {
   class MockIntersectionObserver implements IntersectionObserver {
     readonly root: Element | Document | null = null;
     readonly rootMargin = "";
-    readonly scrollMargin = "";
+    readonly scrollMargin = "0px 0px 0px 0px";
     readonly thresholds: number[] = [];
 
     observe(): void {
